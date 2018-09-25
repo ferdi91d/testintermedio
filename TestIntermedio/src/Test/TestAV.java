d1b600eaace54c48ec23839edd773b1d7e169297
FD_Step <ferdinando.dipalma@stepsrl.org>
2018-09-25 14:37:49 +0200

    initial commit

---
+++ TestIntermedio/src/Test/TestAV.java
@@ -0,0 +1,100 @@
+package Test;
+
+public class TestAV {
+
+	/*
+	 * 1 - Creare una funzione C#, che data una stringa in input capitalizzi la
+	 * prima lettera di ogni parola Es. Input --> flusso di lavoro Output --> Flusso
+	 * Di Lavoro
+	 * 
+	 * 
+	 */
+	public static String capitalizeChars(String input) {
+		return null;
+	}
+
+	/*
+	 * 2 - Creare una funzione C# per estrarre la diagonale di una array
+	 * bidimensionale Input --> int[3,3] array2D = new int[3,3] { { 1, 2, 3 }, { 3,
+	 * 4, 5}, { 5, 6, 7 } }; Output --> 1,4,7
+	 */
+	public static int[] array2dDiagonal(int[][] array2d) {
+		return null;
+	}
+
+	/*
+	 * 3 - Creare una funzione C#, che prende in input due interi e restituisce vero
+	 * se il secondo e' piu' grande del primo Es. Input --> a = 5; b = 9; Output -->
+	 * true
+	 * 
+	 * 
+	 */
+	public static boolean isMajorNumber(int a, int b) {
+
+		return false;
+	}
+
+	/*
+	 * 4 - Creare una funzione C#, che dati i minuti, stampi il numero di ore e di
+	 * minuti corrispondenti Es. Input --> 130 Output --> 2 ore e 10 minuti
+	 */
+	public static void minTohoursmin(int minuti) {
+
+	}
+
+	/*
+	 * 5 - Creare una funzione C#, che data una stringa, ne restituisca l'inverso
+	 * Es. Input--> casa Output--> asac
+	 * 
+	 * 
+	 */
+	public static String reverseString(String input) {
+		return null;
+	}
+
+	/*
+	 * 6 - Creare una funzione C#, che dato un array di interi ne restituisca il
+	 * massimo Es. Input --> int[] myInts = { 5, 10, 15 }; Output --> 15
+	 * 
+	 * 
+	 */
+	public static int getMaxFromArray(int[] array) {
+		return 0;
+	}
+
+	/*
+	 * 7 - Creare una funzione C#, che data una matrice, restituisca la somma di
+	 * ogni riga all'interno di un array Es. Input --> int[3,3] array2D = new
+	 * int[3,3] { { 1, 2, 3 }, { 3, 4, 5}, { 5, 6, 7 } }; Output--> int[] myResults
+	 * = {6, 12, 18};
+	 */
+	public static int[] sumEachRow(int[][] array2d) {
+		return null;
+	}
+
+	/*
+	 * 8 - Creare una funzione C#, che dato in input un char e una stringa, contare
+	 * le occorrenze del dato carattere all'interno della stringa Es. Input --> char
+	 * carattere = 'a'; string input = 'casa'; Output --> 2;
+	 */
+	public static int countChar(char carattere, String input) {
+		return 0;
+	}
+
+	/*
+	 * 9 - Creare una funzione C#, che dato in input una stringa, riconoscere se è
+	 * un numero Input--> string input = '12345' Output --> true
+	 */
+	public static boolean stringIsANumber(String input) {
+
+		return false;
+	}
+
+	/*
+	 * 10 - Creare una funzione C#, che data una stringa, verifica se la stringa
+	 * inserita è palindroma Input --> string input = 'anna'; Output --> true;
+	 */
+	public static boolean stringIsPalindroma(String input) {
+		return false;
+	}
+}
