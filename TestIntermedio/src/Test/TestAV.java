a83e78dff656204017b7db8c94329035f7cc34d5
Ferdinando Di Palma <31693396+ferdi91d@users.noreply.github.com>
2018-10-03 16:38:52 +0200

    Update TestAV.java

--- TestIntermedio/src/Test/TestAV.java
+++ TestIntermedio/src/Test/TestAV.java
@@ -3,7 +3,7 @@ package Test;
 public class TestAV {
 
 	/*
-	 * 1 - Creare una funzione C#, che data una stringa in input capitalizzi la
+	 * 1 - Creare una funzione, che data una stringa in input capitalizzi la
 	 * prima lettera di ogni parola Es. Input --> flusso di lavoro Output --> Flusso
 	 * Di Lavoro
 	 * 
@@ -14,7 +14,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 2 - Creare una funzione C# per estrarre la diagonale di una array
+	 * 2 - Creare una funzione per estrarre la diagonale di una array
 	 * bidimensionale Input --> int[3,3] array2D = new int[3,3] { { 1, 2, 3 }, { 3,
 	 * 4, 5}, { 5, 6, 7 } }; Output --> 1,4,7
 	 */
@@ -23,7 +23,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 3 - Creare una funzione C#, che prende in input due interi e restituisce vero
+	 * 3 - Creare una funzione, che prende in input due interi e restituisce vero
 	 * se il secondo e' piu' grande del primo Es. Input --> a = 5; b = 9; Output -->
 	 * true
 	 * 
@@ -35,7 +35,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 4 - Creare una funzione C#, che dati i minuti, stampi il numero di ore e di
+	 * 4 - Creare una funzione, che dati i minuti, stampi il numero di ore e di
 	 * minuti corrispondenti Es. Input --> 130 Output --> 2 ore e 10 minuti
 	 */
 	public static void minTohoursmin(int minuti) {
@@ -43,7 +43,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 5 - Creare una funzione C#, che data una stringa, ne restituisca l'inverso
+	 * 5 - Creare una funzione, che data una stringa, ne restituisca l'inverso
 	 * Es. Input--> casa Output--> asac
 	 * 
 	 * 
@@ -53,7 +53,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 6 - Creare una funzione C#, che dato un array di interi ne restituisca il
+	 * 6 - Creare una funzione, che dato un array di interi ne restituisca il
 	 * massimo Es. Input --> int[] myInts = { 5, 10, 15 }; Output --> 15
 	 * 
 	 * 
@@ -63,7 +63,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 7 - Creare una funzione C#, che data una matrice, restituisca la somma di
+	 * 7 - Creare una funzione, che data una matrice, restituisca la somma di
 	 * ogni riga all'interno di un array Es. Input --> int[3,3] array2D = new
 	 * int[3,3] { { 1, 2, 3 }, { 3, 4, 5}, { 5, 6, 7 } }; Output--> int[] myResults
 	 * = {6, 12, 18};
@@ -73,7 +73,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 8 - Creare una funzione C#, che dato in input un char e una stringa, contare
+	 * 8 - Creare una funzione , che dato in input un char e una stringa, contare
 	 * le occorrenze del dato carattere all'interno della stringa Es. Input --> char
 	 * carattere = 'a'; string input = 'casa'; Output --> 2;
 	 */
@@ -82,7 +82,7 @@ public class TestAV {
 	}
 
 	/*
-	 * 9 - Creare una funzione C#, che dato in input una stringa, riconoscere se è
+	 * 9 - Creare una funzione , che dato in input una stringa, riconoscere se Ã¨
 	 * un numero Input--> string input = '12345' Output --> true
 	 */
 	public static boolean stringIsANumber(String input) {
@@ -91,8 +91,8 @@ public class TestAV {
 	}
 
 	/*
-	 * 10 - Creare una funzione C#, che data una stringa, verifica se la stringa
-	 * inserita è palindroma Input --> string input = 'anna'; Output --> true;
+	 * 10 - Creare una funzione , che data una stringa, verifica se la stringa
+	 * inserita Ã¨ palindroma Input --> string input = 'anna'; Output --> true;
 	 */
 	public static boolean stringIsPalindroma(String input) {
 		return false;
